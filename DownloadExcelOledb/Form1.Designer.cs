f4c22a78e1af6b518d103858c116dfa6e37fb513
Abhishek Srivastava <abhysri76752@gmail.com>
2024-12-02 23:00:22 +0530

    Add project files.

---
+++ DownloadExcelOledb/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace DownloadExcelOledb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ToDTpicker = new System.Windows.Forms.DateTimePicker();
+            this.FromDTpicker = new System.Windows.Forms.DateTimePicker();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.lblMess = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(40, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 65);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "End Date:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Start Date:";
+            // 
+            // ToDTpicker
+            // 
+            this.ToDTpicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ToDTpicker.Location = new System.Drawing.Point(176, 219);
+            this.ToDTpicker.Name = "ToDTpicker";
+            this.ToDTpicker.Size = new System.Drawing.Size(488, 34);
+            this.ToDTpicker.TabIndex = 6;
+            // 
+            // FromDTpicker
+            // 
+            this.FromDTpicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FromDTpicker.Location = new System.Drawing.Point(176, 179);
+            this.FromDTpicker.Name = "FromDTpicker";
+            this.FromDTpicker.Size = new System.Drawing.Size(488, 34);
+            this.FromDTpicker.TabIndex = 5;
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(0, 0);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(1100, 39);
+            this.lblHeader.TabIndex = 7;
+            this.lblHeader.Text = "Download Report";
+            this.lblHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMess
+            // 
+            this.lblMess.AutoSize = true;
+            this.lblMess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMess.Location = new System.Drawing.Point(283, 437);
+            this.lblMess.Name = "lblMess";
+            this.lblMess.Size = new System.Drawing.Size(164, 25);
+            this.lblMess.TabIndex = 7;
+            this.lblMess.Text = "Please Wait .....";
+            this.lblMess.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "BOTH",
+            "A",
+            "B"});
+            this.comboBox1.Location = new System.Drawing.Point(176, 284);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(153, 37);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 287);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 29);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Shift";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 29);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Cell No";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "All",
+            "CELL1",
+            "CELL2",
+            "CELL3",
+            "CELL4",
+            "CELL5"});
+            this.comboBox2.Location = new System.Drawing.Point(162, 105);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(153, 37);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(26, 57);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(139, 33);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "FC ONLY";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(185, 57);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(138, 33);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.Text = "FB ONLY";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.Location = new System.Drawing.Point(347, 57);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(103, 33);
+            this.checkBox3.TabIndex = 16;
+            this.checkBox3.Text = "BOTH";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 725);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.lblMess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ToDTpicker);
+            this.Controls.Add(this.FromDTpicker);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker ToDTpicker;
+        private System.Windows.Forms.DateTimePicker FromDTpicker;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label lblMess;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ DownloadExcelOledb/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace DownloadExcelOledb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private DataTable GetDataFromSQL(string connectionString, string TableName)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                DateTime StartDate = FromDTpicker.Value;
+                DateTime EndDate = ToDTpicker.Value;
+                EndDate = EndDate.AddDays(1);
+
+
+
+                string query = "";
+                string ResultQuerry = "";
+                string Shift = comboBox1.SelectedItem.ToString();
+                if (Shift == "BOTH")
+                {
+                    ResultQuerry = CaseForQuerry(query,TableName,StartDate,EndDate);
+                    //query = "Select * from " + TableName + " Where MfgDate >= '" + StartDate.ToString("MM/dd/yyyy HH:mm:ss") + "' And MfgDate is not Null MfgDate <= '" + EndDate.ToString("MM/dd/yyyy HH:mm:ss") + "'";
+                   
+                   
+                }
+                else
+                {
+
+                    //query = "Select * from " + TableName + " where Shift = '" + Shift + "' And MfgDate is not Null MfgDate >= '" + StartDate.ToString("MM/dd/yyyy HH:mm:ss") + "' And MfgDate <= '" + EndDate.ToString("MM/dd/yyyy HH:mm:ss") + "'";
+                   ResultQuerry = CaseForQuerryWithShift(query, TableName, StartDate, EndDate,Shift);
+                }
+                using (SqlCommand command = new SqlCommand(ResultQuerry, connection))
+               //using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        dataTable.Load(reader);
+                    }
+                }
+            }
+
+            return dataTable;
+        }
+
+        private  void WriteDataToExcel(DataTable dataTable, string filePath, string sheetName)
+        {
+            sheetName = GetValidSheetName(sheetName);
+
+            string connString = "Provider=Microsoft.ACE.OLEDB.16.0;Data Source=" + filePath + ";Extended Properties=\"Excel 12.0 Xml;HDR=YES;\"";
+
+            using (OleDbConnection conn = new OleDbConnection(connString))
+            {
+                conn.Open();
+
+                OleDbCommand command = new OleDbCommand("CREATE TABLE [" + sheetName + "] (", conn);
+
+                foreach (DataColumn column in dataTable.Columns)
+                {
+                    command.CommandText += "[" + column.ColumnName + "] TEXT,";
+                }
+
+                command.CommandText = command.CommandText.TrimEnd(',') + ")";
+                command.ExecuteNonQuery();
+
+                // Insert data into the worksheet
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    command = new OleDbCommand("INSERT INTO [" + sheetName + "$] (", conn);
+                    foreach (DataColumn column in dataTable.Columns)
+                    {
+                        command.CommandText += "[" + column.ColumnName + "],";
+                    }
+
+                    command.CommandText = command.CommandText.TrimEnd(',') + ") VALUES (";
+
+                    foreach (var item in row.ItemArray)
+                    {
+                        command.CommandText += "'" + item.ToString().Replace("'", "''") + "',"; // Handle single quotes in data
+                    }
+
+                    command.CommandText = command.CommandText.TrimEnd(',') + ")";
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            
+        }
+
+        private static string GetValidSheetName(string name)
+        {
+            string validName = name.Substring(8) ;
+           
+            return validName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            lblMess.Text = "";
+            lblMess.Visible = true;
+            lblMess.Text="Please Wait...";
+
+            string ConnString = "data source=PMA-L-3;initial catalog=YY81;persist security info=True;user id=sa;password=pmal@123;";
+            List<string> tableNames;
+
+            if (checkBox3.Checked)
+            {
+                tableNames = new List<string>()
+                {
+                    "SequenceFB_SingleRH_Mig2",
+                    "SequenceFB_SingleLH_Mig2",
+                    "SequenceFB_DoubleRH_Mig1",
+                    "SequenceFB_DoubleLH_Mig1",
+                    "SequenceFC_NonLifterDoubleLH_Mig3",
+                    "SequenceFC_LifterSingleRH_Mig5",
+                    "SequenceFC_LifterDoubleRH_Mig5",
+                    "SequenceFC_LifterDoubleLH_Mig5",
+                    "SequenceFC_NonLiftersSingleLH_Mig3",
+                    "SequenceFC_NonLifterDoubleRH_Mig4",
+                };
+            }
+            else if(checkBox1.Checked)
+                {
+                tableNames = new List<string>()
+                {
+                    "SequenceFC_NonLifterDoubleLH_Mig3",
+                    "SequenceFC_LifterSingleRH_Mig5",
+                    "SequenceFC_LifterDoubleRH_Mig5",
+                    "SequenceFC_LifterDoubleLH_Mig5",
+                    "SequenceFC_NonLiftersSingleLH_Mig3",
+                    "SequenceFC_NonLifterDoubleRH_Mig4",
+                };
+                }
+            else if (checkBox2.Checked)
+            {
+                tableNames = new List<string>()
+                {
+                    "SequenceFB_SingleRH_Mig2",
+                    "SequenceFB_SingleLH_Mig2",
+                    "SequenceFB_DoubleRH_Mig1",
+                    "SequenceFB_DoubleLH_Mig1",
+                };
+            }
+            else
+            {
+                tableNames = new List<string>()
+                {
+                    "SequenceFB_SingleRH_Mig2",
+                    "SequenceFB_SingleLH_Mig2",
+                    "SequenceFB_DoubleRH_Mig1",
+                    "SequenceFB_DoubleLH_Mig1",
+                    "SequenceFC_NonLifterDoubleLH_Mig3",
+                    "SequenceFC_LifterSingleRH_Mig5",
+                    "SequenceFC_LifterDoubleRH_Mig5",
+                    "SequenceFC_LifterDoubleLH_Mig5",
+                    "SequenceFC_NonLiftersSingleLH_Mig3",
+                    "SequenceFC_NonLifterDoubleRH_Mig4",
+                };
+            }
+            try
+            {
+                if (comboBox2.SelectedItem.ToString() != "All")
+                {
+                    string KeepStr = comboBox2.SelectedItem.ToString().Replace("CELL", "");
+                    for (int i = 0; i <= tableNames.Count; i++)
+                    {
+                        if (!tableNames[i].Contains(KeepStr))
+                        { tableNames.RemoveAt(i); i--; }
+                    }
+                }
+            }
+            catch { }
+
+           
+
+
+            using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "Excel Workbook|*.xlsx" })
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        foreach (string tableName in tableNames)
+                        {
+                            DataTable dataTable = GetDataFromSQL(ConnString, tableName);
+                            WriteDataToExcel(dataTable, sfd.FileName, tableName);
+                        }
+                        lblMess.Text = "Report Generated successfully";
+                        MessageBox.Show("Data has been successfully exported to Excel.", "Export Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkBox3.Checked = true;
+            comboBox2.SelectedIndex = 0;
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox3.Checked = false;
+            checkBox2.Checked = false;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox3.Checked = false;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+        }
+
+        private string CaseForQuerry(string query, string TableName, DateTime StartDate, DateTime EndDate)
+        {
+            if (TableName == "SequenceFB_DoubleLH_Mig1" || TableName == "SequenceFB_DoubleRH_Mig1" || TableName == "SequenceFB_SingleLH_Mig2" || TableName == "SequenceFB_SingleRH_Mig2")
+            {
+                                                                                                                                                                                                                              //query = "Select * from " + TableName + " Where MfgDate >= '" + StartDate.ToString("MM/dd/yyyy HH:mm:ss") + "' And MfgDate is not Null MfgDate <= '" + EndDate.ToString("MM/dd/yyyy HH:mm:ss") + "'";
+                query = "Select Id,Date, Shift,SequenceNo,InnerRecliner,OuterRecliner,MfgDate,BuiltTktDate,FinalBarCode,EngravingData,InspectionStation as 'Mig Visual InspectionStation' ,CrimpingStatus,SpiralStatus,PrformanceTest, Gaugingstation, Rework,ReworkDone,Reject,RejectAt,Station from " + TableName + " Where MfgDate >= '" + StartDate.ToString("MM/dd/yyyy HH:mm:ss") + "' And MfgDate is not Null and MfgDate <= '" + EndDate.ToString("MM/dd/yyyy HH:mm:ss") + "'";
+            }
+
+
+            else if (TableName == "SequenceFC_NonLifterDoubleLH_Mig3" || TableName == "SequenceFC_NonLifterDoubleRH_Mig4" || TableName == "SequenceFC_NonLiftersSingleLH_Mig3")
+            {
+                query = "Select Id,Date,Shift,SequenceNo,MfgDate,BuiltTktDate,FinalBarCode,EngravingData,InspectionStation as 'Mig Visual InspectionStation',GaugingStation,Rework,ReworkDone,Reject,RejectAt,Station from " + TableName + " Where MfgDate >= '" + StartDate.ToString("MM/dd/yyyy HH:mm:ss") + "' And MfgDate is not Null and MfgDate <= '" + EndDate.ToString("MM/dd/yyyy HH:mm:ss") + "'";
+            }
+
+            else if (TableName == "SequenceFC_LifterDoubleLH_Mig5" || TableName == "SequenceFC_LifterDoubleRH_Mig5" || TableName == "SequenceFC_LifterSingleRH_Mig5")
+            {
+                query = "Select Id,Date,Shift,SequenceNo,MfgDate,BuiltTktDate,FinalBarCode, EngravingData, InspectionStation as 'Mig Visual InspectionStation' ,FlaringStatus,GaugingStation,Rework,ReworkDone,Reject,RejectAt,Station,MigGauging,Torque1,Torque2,Torque3,Torque4,Torque5,BreakAssyStatus from " + TableName + " Where MfgDate >= '" + StartDate.ToString("MM/dd/yyyy HH:mm:ss") + "' And MfgDate is not Null and MfgDate <= '" + EndDate.ToString("MM/dd/yyyy HH:mm:ss") + "'";
+            }
+            return query;
+        }
+
+
+        private string CaseForQuerryWithShift(string query, string TableName, DateTime StartDate, DateTime EndDate, string Shift)
+        {
+            if (TableName == "SequenceFB_DoubleLH_Mig1" || TableName == "SequenceFB_DoubleRH_Mig1" || TableName == "SequenceFB_SingleLH_Mig2" || TableName == "SequenceFB_SingleRH_Mig2")
+            {
+                query = "Select Id,Date, Shift,SequenceNo,InnerRecliner,OuterRecliner,MfgDate,BuiltTktDate,FinalBarCode,EngravingData,InspectionStation as 'Mig Visual InspectionStation' ,CrimpingStatus,SpiralStatus,PrformanceTest, Gaugingstation, Rework,ReworkDone,Reject,RejectAt,Station from " + TableName + " Where Shift = '" + Shift + "' and MfgDate >= '" + StartDate.ToString("MM/dd/yyyy HH:mm:ss") + "' And MfgDate is not Null and MfgDate <= '" + EndDate.ToString("MM/dd/yyyy HH:mm:ss") + "'";
+            }
+
+
+            else if (TableName == "SequenceFC_NonLifterDoubleLH_Mig3" || TableName == "SequenceFC_NonLifterDoubleRH_Mig4" || TableName == "SequenceFC_NonLiftersSingleLH_Mig3")
+            {
+                query = "Select Id,Date,Shift,SequenceNo,MfgDate,BuiltTktDate,FinalBarCode,EngravingData,InspectionStation as 'Mig Visual InspectionStation',GaugingStation,Rework,ReworkDone,Reject,RejectAt,Station from " + TableName + " Where Shift = '" + Shift + "' and MfgDate >= '" + StartDate.ToString("MM/dd/yyyy HH:mm:ss") + "' And MfgDate is not Null and MfgDate <= '" + EndDate.ToString("MM/dd/yyyy HH:mm:ss") + "'";
+            }
+
+            else if (TableName == "SequenceFC_LifterDoubleLH_Mig5" || TableName == "SequenceFC_LifterDoubleRH_Mig5" || TableName == "SequenceFC_LifterSingleRH_Mig5")
+            {
+                query = "Select Id,Date,Shift,SequenceNo,MfgDate,BuiltTktDate,FinalBarCode, EngravingData, InspectionStation as 'Mig Visual InspectionStation' ,FlaringStatus,GaugingStation,Rework,ReworkDone,Reject,RejectAt,Station,MigGauging,Torque1,Torque2,Torque3,Torque4,Torque5,BreakAssyStatus from " + TableName + " Where Shift = '" + Shift + "' and MfgDate is not Null and MfgDate >= '" + StartDate.ToString("MM/dd/yyyy HH:mm:ss") + "' And MfgDate <= '" + EndDate.ToString("MM/dd/yyyy HH:mm:ss") + "'";
+            }
+            return query;
+
+        }
+
+
+
+
+        }
+
+    }
+
